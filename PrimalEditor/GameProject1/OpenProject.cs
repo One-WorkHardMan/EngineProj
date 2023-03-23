2187f1d638b49ef0aaf22f86dafebde9d7a8e6e2
liuzhouJAVA <lzzl189178>
2023-03-23 18:36:49 +0800

    004

--- PrimalEditor/GameProject1/OpenProject.cs
+++ PrimalEditor/GameProject1/OpenProject.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -8,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace PrimalEditor.GameProject1
 {
+    // 项目的创建数据，主要是路径和打开时间？
     [DataContract]
     public class ProjectData {
         [DataMember]
@@ -16,7 +19,10 @@ namespace PrimalEditor.GameProject1
         public string ProjectPath { get; set; }
         [DataMember]
         public DateTime Date { get; set; }
-
+        [DataMember]
+        public string FullPath { get => $"{ProjectPath}{ProjectName}{Project.Extension}"; }
+        public Byte[] Icon { get; set; }
+        public Byte[] Screenshot { get; set; }
     }
     [DataContract]
     public class ProjectDataList {
@@ -27,19 +33,56 @@ namespace PrimalEditor.GameProject1
 
     class OpenProject
     {
-/*        private static readonly string _applicationDataPath = $@"{Environment}";
+        private static readonly string _applicationDataPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\PrimalEditor\";
+        private static readonly string _projectDataPath;
+        //一个私有的不可见只读的 可观测集合；
+        private static readonly ObservableCollection<ProjectData> _projects = new ObservableCollection<ProjectData>();
+        //对上面的集合进行封装，一个共有的集合；
+        public static ReadOnlyObservableCollection<ProjectData> Projects { get; }
+
+        // 读项目的 创建数据
+        public static void ReadProjectData() {
+
+        }
+        private static void WriteProjectData()
+        {
+            
+        }
+        // 写项目的 创建数据
+        public static Project Open(ProjectData data) {
+            ReadProjectData();
+            
+            // 比较最近打开文件的路径和我现在打开的路径是否相同？相同就返回集合的第一个数据element
+            var project = _projects.FirstOrDefault(x => x.FullPath == data.FullPath);
+            if (project!=null) {
+                project.Date = DateTime.Now;
+            }
+            else {
+                project = data;
+                project.Date = DateTime.Now;
+                _projects.Add(project);
+            }
+            WriteProjectData();
+            return null;
+        }
 
 
-        static OpenProject() {
+        //静态构造函数只执行一次
+        static OpenProject()
+        {
             try
             {
-                if (!Directory.Exists()) ;
+                // 判断文件夹是否存在
+                if (!Directory.Exists(_applicationDataPath)) Directory.CreateDirectory(_applicationDataPath);
+                _projectDataPath = $@"{_applicationDataPath}\ProjectData.xml";
+                Projects = new ReadOnlyObservableCollection<ProjectData>(_projects);
+                ReadProjectData();
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
+                Debug.WriteLine(ex.Message);
                 throw;
             }
-        }*/
+        }
     }
 }
