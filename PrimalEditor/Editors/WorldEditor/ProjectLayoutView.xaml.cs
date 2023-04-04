bb4e66539925d23a8b1a2c75050cbefae85a6314
liuzhouJAVA <lzzl189178>
2023-04-04 11:20:42 +0800

    2023年4月4日11:20:46

---
+++ PrimalEditor/Editors/WorldEditor/ProjectLayoutView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using PrimalEditor.GameProject1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace PrimalEditor.Editors
+{
+    /// <summary>
+    /// ProjectLayoutView.xaml 的交互逻辑
+    /// </summary>
+    public partial class ProjectLayoutView : UserControl
+    {
+        public ProjectLayoutView()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClick(object sender,RoutedEventArgs e) { 
+            var vm = DataContext as Project;
+            
+        }
+    }
+}
